44fa7c7224ee66da2a3887f41af0fd995e691b7b
Kwan Lowe <kwan@digitalhermit.com>
2020-04-07 09:21:45 -0400

    FIx it

--- linux/robotframework/install_chromedriver.sh
+++ linux/robotframework/install_chromedriver.sh
@@ -7,6 +7,7 @@ CHROMEDRIVER_URL=https://chromedriver.storage.googleapis.com
 CHROME_VERSION=$(google-chrome --version)
 CHROME_VERSION_MAJOR=$(echo $CHROME_VERSION|awk -F '[. /]*'  '{print $3}' )
 CHROMEDRIVER_VERSION=$(curl -s ${CHROMEDRIVER_URL}/LATEST_RELEASE_${CHROME_VERSION_MAJOR})
-CHROMEDRIVER_BINARY=${CD_URL}/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
-curl -o bin/chromedriver ${CHROMEDRIVER_BINARY}
-chmod +x bin/chromedriver
+CHROMEDRIVER_BINARY=${CHROMEDRIVER_URL}/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
+curl -o bin/chromedriver.zip "${CHROMEDRIVER_BINARY}"
+unzip -d bin/ bin/chromedriver.zip
+chmod +x bin/chromedriver 
