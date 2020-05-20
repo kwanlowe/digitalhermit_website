3420980892898a93f5f134866078a19f2f3a1f94
Kwan Lowe <kwan@canterbury.digitalhermit.com>
2020-05-20 08:47:10 -0400

    Raise threshold

--- linux/arduino/micropython/solder_fan.py
+++ linux/arduino/micropython/solder_fan.py
@@ -8,7 +8,7 @@ def toggle(p):
 disabled=1
 enabled=0
 
-pollution_threshold = 500
+pollution_threshold = 600
 
 led1 = machine.Pin(2, machine.Pin.OUT)
 led2 = machine.Pin(16, machine.Pin.OUT)
@@ -22,11 +22,11 @@ while True:
     print("Sensor reading:", sensor.read())
     sensor_reading = sensor.read()
     if sensor_reading > pollution_threshold:
-        # relay.on()
+        relay.on()
         print("Sensor reading above threshold. Enabling fan.")
         led2.value(enabled)
     else:
-        # relay.off()
+        relay.off()
         led2.value(disabled)
         print("Sensor reading below threshold. Disabling fan.")
     time.sleep_ms(8000)
